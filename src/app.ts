5c5a0be02d821777c7593cba64e292d7533f7ce3
Daniel <danieldcc.colares@gmail.com>
2026-08-17 16:13:03 -0300

    feat: adds global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
+import z, { ZodError } from 'zod'
+import { env } from './env'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: z.treeifyError(error),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    //TODO: adicionar uma ferramenta externa de monitoramento DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})

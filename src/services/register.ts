0fb5dece0ae4b91cde07f85fa385674eca49ac85
Daniel <danieldcc.colares@gmail.com>
2026-08-17 14:11:06 -0300

    chore: repository pattern

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users.repository'
 
 interface RegisterServiceRequest {
   name: string
@@ -22,11 +23,12 @@ export async function registerService({
     throw new Error('Email already exist')
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  // Repository Pattern
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }

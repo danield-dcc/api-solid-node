5b90cb0ef998c3cdbaa98af25973052c7e4ddc6d
Daniel <danieldcc.colares@gmail.com>
2026-08-17 12:06:58 -0300

    feat: user creation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { env } from '@/env'
-import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaClient } from '../../prisma/generated/prisma/client'
 
 const connectionString = `${env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({
+  adapter,
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
 
 export { prisma }

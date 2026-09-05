93f46575a7b83d0f73c0d922c1c0687a8c9d2a66
Daniel <danieldcc.colares@gmail.com>
2026-09-05 12:36:48 -0300

    chore: adds check ins repository

--- src/repositories/prisma/prisma-users.repository.ts
+++ src/repositories/prisma/prisma-users.repository.ts
@@ -14,6 +14,16 @@ export class PrismaUsersRepository implements UsersRepository {
     return user
   }
 
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: { email },
